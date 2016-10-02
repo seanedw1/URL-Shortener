edbaef6b5216c003e90e49fd36d5e1a71c85b15c
seanedw1 <sedwards@fullsail.edu>
2016-10-02 15:55:13 -0400

    finised user model starting route

--- src/models/user.js
+++ src/models/user.js
@@ -9,3 +9,37 @@ exports.create = (payload, err, success) => {
 exports.findAll = (err, success) => {
   db.user.findAll().then(success).catch(err);
 };
+
+// find by id
+exports.find = (payload, err, success) => {
+  db.user.find({
+    where: {
+      id: payload.id,
+    },
+    // find all relations in sequelize
+    include: [{
+      all: true,
+      nested: true,
+    }],
+  }).then(success).catch(err);
+};
+
+// update
+exports.update = (payload, err, success) => {
+  db.user.find({
+    where: {
+      id: payload.id,
+    },
+  }).then((existingData) => {
+    existingData.updateAttributes(payload).then(success).catch(err);
+  }).catch(err);
+};
+
+// delete
+exports.destroy = (payload, err, success) => {
+  db.user.destroy({
+    where: {
+      id: payload.id,
+    },
+  }).then(success).catch(err);
+};
