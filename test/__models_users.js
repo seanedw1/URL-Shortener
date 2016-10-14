e1efd3d00313388c0ea7dc4c4ebba85a1a169de0
seanedw1 <sedwards@fullsail.edu>
2016-10-14 16:23:55 -0400

    debugging read user by id unit test

--- test/__models_users.js
+++ test/__models_users.js
@@ -1,28 +1,55 @@
 const expect = require('chai').expect;
 const User = require('../src/models/user');
+const gen = require('../src/models/gen');
 
 describe('user model', () => {
+  // fake test user
+  let testUser = {
+    name: 'testUser',
+    username: 'simpsonsfan',
+    password: gen.generateHash('password'),
+    tokenreq: true,
+  };
+
   // Create user
   it('Create user', (done) => {
-// fake test user
-    const testUser = {
-      name: 'testUser',
-      username: 'simpsonsfan',
-      password: 'testpass',
-      tokenreq: true,
-    };
 // create method
     User.create(testUser, (err) => {
-      throw new Error(err);
+      if (err) throw err;
     }, (user) => {
       expect(user.name).to.be.equal(testUser.name);
+      expect(user.username).to.be.equal(testUser.username);
       done();
     }
     );
   });
 
-  it('Read all users', () => {});
-  it('Read user by id', () => {});
+  // read all users
+  it('Read all users', (done) => {
+    // read all method
+    User.findAll((err) => {
+      if (err) throw err;
+      // sucess callback
+    }, (users) => {
+      this.testUsers = users;
+      expect(this.testUsers.length).to.be.above(0);
+      done();
+    }
+  );
+  });
+
+  // Read user by id
+  it('Read user by id', (done) => {
+  // read by id method
+    User.find(testUser, (err) => {
+      if (err) throw err;
+    }, (user) => {
+      expect(user.name).to.be.equal(testUser.name);
+      done();
+    }
+  );
+  });
+
   it('Update user', () => {});
   it('Delete user', () => {});
 });
