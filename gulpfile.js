537c77a59529eae485affd40748bfb60c7030b39
seanedw1 <sedwards@fullsail.edu>
2016-10-20 21:53:15 -0400

    gulp file configured

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,15 @@
 const gulp = require('gulp');
+
+gulp.task('major', () => {
+  console.log('lo4');
+});
+
+gulp.task('minor', () => {
+  console.log('lo4l');
+});
+
+gulp.task('patch', () => {
+  console.log('lo4');
+});
+
+gulp.task('default', ['major', 'minor', 'patch']);
